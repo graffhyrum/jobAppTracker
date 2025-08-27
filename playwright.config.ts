878c6dad1c8f99e768088fef6936a0e741724a7c
Joshua Pendragon <pendragon.josh@gmail.com>
2025-08-27 11:53:42 -0500

    refactor(config): update HTML reporter to never open automatically
    - Changed Playwright HTML reporter to prevent automatic opening.

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
 	forbidOnly: !!process.env.CI,
 	retries: process.env.CI ? 2 : 0,
 	workers: process.env.CI ? 1 : undefined,
-	reporter: [["html", { open: "on-failure" }]],
+	reporter: [["html", { open: "never" }]],
 
 	use: {
 		baseURL,
